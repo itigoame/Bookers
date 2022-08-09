1e47fb91ae50eba12778a6902dcd6148baa1229b
itigoame <rikuan1128@gmail.com>
2022-08-09 09:39:33 +0000

    [add]showpage

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,13 +1,27 @@
 class BookersController < ApplicationController
-  def new
+
+  def create
+    booker=Booker.new(booker_params)
+    booker.save
+    redirect_to booker_path(booker.id)
   end
 
   def index
+    @bookers=Booker.all
+    @booker=Booker.new
   end
 
   def show
+    @booker=Booker.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def booker_params
+    params.require(:booker).permit(:title,:body)
+  end
+
 end
