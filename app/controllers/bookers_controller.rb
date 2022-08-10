0f3fbe5b4a368f0487d93ae2dc9ca31946e95807
itigoame <rikuan1128@gmail.com>
2022-08-10 08:34:31 +0000

    [add]

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,9 +1,14 @@
 class BookersController < ApplicationController
 
   def create
-    booker=Booker.new(booker_params)
-    booker.save
-    redirect_to booker_path(booker.id)
+    @booker=Booker.new(booker_params)
+
+    if @booker.save
+      redirect_to booker_path(@booker.id), notice: 'Book was successfully created.'
+    else
+      @bookers=Booker.all
+      render :index
+    end
   end
 
   def index
@@ -16,6 +21,23 @@ class BookersController < ApplicationController
   end
 
   def edit
+    @booker=Booker.find(params[:id])
+  end
+
+  def update
+    @booker=Booker.find(params[:id])
+
+    if @booker.update(booker_params)
+      redirect_to booker_path(@booker.id), notice: 'Book was successfully created.'
+    else
+      render:edit
+    end
+  end
+
+  def destroy
+    booker=Booker.find(params[:id])
+    booker.destroy
+    redirect_to bookers_path
   end
 
   private
